e7c058dbc80bfb56331c2ef0299e860526a2c579
ziga <4okayz07@solent.ac.uk>
2021-02-08 10:20:19 +0000

    completed q1

---
+++ week1/main.py
@@ -0,0 +1,10 @@
+def q1():
+    print("who broke my heart?")
+    response = input()
+    print(f"my first love {response} broke my heart for the first time")
+
+q1()
+
+
+def q2():
+    print("who sees my rolling? (cops,wardens,other)")
