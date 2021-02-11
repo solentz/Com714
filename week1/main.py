2d3e20f13bbbf9294f57ed35986cb5b1cfd62de3
ziga <4okayz07@solent.ac.uk>
2021-02-11 08:23:59 +0000

    q3

--- week1/main.py
+++ week1/main.py
@@ -12,3 +12,25 @@ def q2():
     print(f"they see my rolling.they hating.they hoping they gonna catch me riding dirty! {response} my music's so loud.i'm swanging.they hoping they gonna catch me riding dirty!")
 
 q2()
+
+
+def q3():
+    num_list = []
+    break_free = input("how many times should i break free:")
+    break_free = int(break_free)
+    print("i'm stronger than i have been before...")
+    print(break_free)
+    for i in range(break_free):
+        num_list.append(i)
+
+    num_list.reverse()
+    for j in num_list:
+        print(f"{j}: this is the part when i break free....")
+        if j == 1:
+            break
+
+    print("cause i can't resist it no more")
+
+
+q3()
+
