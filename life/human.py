1ae189f2146e616f685261dbc7cb98bfe7f2aac2
ziga <4okayz07@solent.ac.uk>
2021-02-11 10:45:28 +0000

    class human

--- life/human.py
+++ life/human.py
@@ -2,6 +2,6 @@ class Human:
 
     MAX_ENERGY = 100
 
-    def __init__(self, name:str, age:int=0) -> None:
+    def __init__(self, name:str, age:int=32) -> None:
         self.name = name
         self.age = age
